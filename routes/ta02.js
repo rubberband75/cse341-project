45a2f96970df2ea7003e4331d1d8df2748f091bc
Chandler Childs <chandler@advenna.com>
2021-04-29 18:15:08 -0600

    add ta02

--- routes/ta02.js
+++ routes/ta02.js
@@ -3,13 +3,56 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req, res, next) => {
-    res.render('pages', { 
-        title: 'Team Activity 02', 
+let users = ['user1', 'user2', 'user3'];
+
+router.get('/', (req, res, next) => {
+    res.render('pages/ta02', {
+        title: 'Team Activity 02',
         path: '/ta02', // For pug, EJS 
-        activeTA03: true, // For HBS
-        contentCSS: true, // For HBS
+        users: users,
+        error: null
     });
 });
+
+router.post('/addUser', (req, res, next) => {
+    const username = req.body.username;
+
+    errorMessage = null;
+    let userExists = (users.indexOf(username) != -1);
+
+    if(userExists) {
+        errorMessage = "User Already Exists"
+    } else {
+        users.push(username)
+    }
+
+    res.render('pages/ta02', {
+        title: 'Team Activity 02',
+        path: '/ta02', // For pug, EJS 
+        users: users,
+        error: errorMessage
+    });
+})
+
+router.post('/removeUser', (req, res, next) => {
+    const username = req.body.username;
+    errorMessage = null;
+    
+    errorMessage = null;
+    let userExists = (users.indexOf(username) != -1);
+
+    if(userExists) {
+        users = users.filter(e => e !== username);
+    } else {
+        errorMessage = "User Not Found"
+    }
+
+    res.render('pages/ta02', {
+        title: 'Team Activity 02',
+        path: '/ta02', // For pug, EJS  
+        users: users,
+        error: errorMessage
+    });
+})
 
 module.exports = router;
