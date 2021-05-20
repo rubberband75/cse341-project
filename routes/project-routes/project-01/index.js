d1da870156ec9ecdb52682b1a90b9247654c81b0
Chandler Childs <chandler@advenna.com>
2021-05-20 11:29:24 -0600

    save cart item total to to res.locals

--- routes/project-routes/project-01/index.js
+++ routes/project-routes/project-01/index.js
@@ -5,11 +5,16 @@ const User = require('../../../models/project-models/project-01/user');
 
 router.use((req, res, next) => {
     if (!req.session.user) {
+        res.locals.cartCount = 0;
         return next();
     }
     User.findById(req.session.user._id)
         .then(user => {
             req.user = user;
+            
+            // Find the total number of items in the cart
+            res.locals.cartCount = user.cart.items.reduce((count, cartItem) => { return count + cartItem.quantity }, 0);
+            
             next();
         })
         .catch(err => console.log(err));
