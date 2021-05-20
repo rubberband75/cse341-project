b81161b9eadd2a5f1589bf4c3d68ef7b2d234c8f
Chandler Childs <chandler@advenna.com>
2021-05-20 12:37:14 -0600

    update cart view

--- models/project-models/project-01/order.js
+++ models/project-models/project-01/order.js
@@ -7,13 +7,15 @@ const orderSchema = new Schema({
         quantity: { type: Number, required: true }
     }],
     user: {
-        name: { type: String, required: true },
+        name: { type: String, required: false },
+        email: { type: String, required: false },
         userId: {
             type: Schema.Types.ObjectId,
             required: true,
             ref: 'User'
         }
-    }
+    },
+    date: { type: Date, required: true, default: Date.now }
 });
 
 
