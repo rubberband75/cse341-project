31cb6d1faa7ca61e1e938d93faf1a58552524801
Chandler Childs <chandler@advenna.com>
2021-05-20 14:41:45 -0600

    carousel images load from db

--- controllers/project-controllers/project-01/index.js
+++ controllers/project-controllers/project-01/index.js
@@ -1,3 +1,13 @@
+const Product = require('../../../models/project-models/project-01/product')
+
 exports.getIndex = (req, res, next) => {
-    res.render('project-views/project-01', { title: 'Project 01', path: '/project/01' });
+    Product.find()
+        .then(products => {
+            res.render('project-views/project-01', {
+                title: 'Project 01',
+                path: '/project/01',
+                products: products
+            });
+        })
+        .catch(err => console.error(err));
 };
