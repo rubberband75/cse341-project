58ca8310f9677c046313e33735a2331807037fd7
Chandler Childs <chandler@advenna.com>
2021-05-20 23:27:18 -0600

    load images as image file

--- routes/project-routes/project-01/index.js
+++ routes/project-routes/project-01/index.js
@@ -3,6 +3,8 @@ const indexController = require('../../../controllers/project-controllers/projec
 const isAuth = require('../../../middlewares/is-auth');
 
 const User = require('../../../models/project-models/project-01/user');
+const Image = require('../../../models/project-models/project-01/image');
+const image = require('../../../models/project-models/project-01/image');
 
 router.use((req, res, next) => {
     if (!req.session.user) {
@@ -12,10 +14,10 @@ router.use((req, res, next) => {
     User.findById(req.session.user._id)
         .then(user => {
             req.user = user;
-            
+
             // Find the total number of items in the cart
             res.locals.cartCount = user.cart.items.reduce((count, cartItem) => { return count + cartItem.quantity }, 0);
-            
+
             next();
         })
         .catch(err => console.log(err));
@@ -26,6 +28,16 @@ router.use(require('./shop'));
 
 router.use('/admin', isAuth, require('./admin'));
 router.use('/account', isAuth, require('./account'));
+
+router.get('/images/:imageID', (req, res, next) => {
+    const { imageID } = req.params;
+    Image.findById(imageID)
+        .then(image => {
+            res.contentType(image.mimetype);
+            res.send(Buffer.from(image.buffer, 'binary'))
+        })
+        .catch(err => console.error(err));
+})
 
 router.get('/', indexController.getIndex);
 
