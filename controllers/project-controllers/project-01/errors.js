f8ea028bb53caa58ef3a4f6a3666c7b7ccdf1d8d
Chandler Childs <chandler@advenna.com>
2021-05-27 22:23:32 -0600

    add 500 error page

--- controllers/project-controllers/project-01/errors.js
+++ controllers/project-controllers/project-01/errors.js
@@ -1,5 +1,4 @@
 exports.get404 = (req, res, next) => {
-    console.log(req)
     res.statusCode = 404;
     res.render('project-views/project-01/errors/404', {
         title: 'Account',
--- routes/project-routes/project-01/index.js
+++ routes/project-routes/project-01/index.js
@@ -15,8 +15,27 @@ router.use("/images", require("./images"));
 router.use("/admin", isAuth, require("./admin"));
 router.use("/account", isAuth, require("./account"));
 
+router.use("/error/:errorCode", (req, res, next) => {
+  const errorCode = req.params.errorCode;
+  let error = new Error(`Error Code Test: ${errorCode}`);
+  error.httpStatusCode = errorCode;
+  next(error);
+});
+
 router.get("/", indexController.getIndex);
 
 router.use(errorController.get404);
+
+router.use((error, req, res, next) => {
+  console.error(error);
+  res
+    .status(error.httpStatusCode ?? 500)
+    .render("project-views/project-01/errors/500", {
+      title: "Account",
+      user: req.user,
+      path: req.originalUrl,
+      errorCode: error.httpStatusCode ?? 500,
+    });
+});
 
 module.exports = router;
