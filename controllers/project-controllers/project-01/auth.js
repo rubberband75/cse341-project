5085a1e05da0124ddab7c664f381ae1cb0ef4999
Chandler Childs <chandler@advenna.com>
2021-05-28 01:16:19 -0600

    add reset page

--- controllers/project-controllers/project-01/auth.js
+++ controllers/project-controllers/project-01/auth.js
@@ -1,109 +1,122 @@
-const bcrypt = require('bcryptjs');
-const nodemailer = require('nodemailer')
-const sendgridTransport = require('nodemailer-sendgrid-transport')
+const bcrypt = require("bcryptjs");
+const nodemailer = require("nodemailer");
+const sendgridTransport = require("nodemailer-sendgrid-transport");
 
-const User = require('../../../models/project-models/project-01/user');
+const User = require("../../../models/project-models/project-01/user");
 
-const transporter = nodemailer.createTransport(sendgridTransport({
+const transporter = nodemailer.createTransport(
+  sendgridTransport({
     auth: {
-        api_key: process.env.SENDGRID_API_KEY
-    }
-}));
+      api_key: process.env.SENDGRID_API_KEY,
+    },
+  })
+);
 
 exports.getLogin = (req, res, next) => {
-    res.render('project-views/project-01/auth/login', {
-        title: 'Login',
-        path: '/project/01/login',
-        errorMessages: req.flash('error')
-    });
+  res.render("project-views/project-01/auth/login", {
+    title: "Login",
+    path: "/project/01/login",
+    errorMessages: req.flash("error"),
+  });
 };
 
 exports.getSignup = (req, res, next) => {
-    res.render('project-views/project-01/auth/signup', {
-        title: 'Sign Up',
-        path: '/project/01/signup',
-        errorMessages: req.flash('error')
-    });
+  res.render("project-views/project-01/auth/signup", {
+    title: "Sign Up",
+    path: "/project/01/signup",
+    errorMessages: req.flash("error"),
+  });
 };
 
 exports.postLogin = (req, res, next) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    User.findOne({ email: email })
-        .then(user => {
-            if (!user) {
-                req.flash('error', 'Invalid email or password');
-                return res.redirect('/project/01/login');
-            }
-            bcrypt
-                .compare(password, user.password)
-                .then(doMatch => {
-                    if (doMatch) {
-                        req.session.isLoggedIn = true;
-                        req.session.user = user;
-                        return req.session.save(err => {
-                            if (err) console.log(err);
-                            res.redirect('/project/01/');
-                        });
-                    } else {
-                        req.flash('error', 'Invalid email or password');
-                    }
-                    res.redirect('/project/01/login');
-                })
-                .catch(err => {
-                    console.log(err);
-                    res.redirect('/project/01/login');
-                });
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        req.flash("error", "Invalid email or password");
+        return res.redirect("/project/01/login");
+      }
+      bcrypt
+        .compare(password, user.password)
+        .then((doMatch) => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save((err) => {
+              if (err) console.log(err);
+              res.redirect("/project/01/");
+            });
+          } else {
+            req.flash("error", "Invalid email or password");
+          }
+          res.redirect("/project/01/login");
         })
-        .catch((err) => next(new Error(err)));
+        .catch((err) => {
+          console.log(err);
+          res.redirect("/project/01/login");
+        });
+    })
+    .catch((err) => next(new Error(err)));
 };
 
 exports.postSignup = (req, res, next) => {
-    const email = req.body.email;
-    const password = req.body.password;
-    const confirmPassword = req.body.confirmPassword;
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
 
-    if (password !== confirmPassword) {
-        req.flash('error', 'Passwords do not match');
-    }
+  if (password !== confirmPassword) {
+    req.flash("error", "Passwords do not match");
+  }
 
-    User.findOne({ email: email })
-        .then(userDoc => {
-            if (userDoc) {
-                req.flash('error', 'E-Mail already in use');
-                return res.redirect('/project/01/signup');
-            }
-            if (password !== confirmPassword) {
-                return res.redirect('/project/01/signup');
-            }
-            return bcrypt
-                .hash(password, 12)
-                .then(hashedPassword => {
-                    const user = new User({
-                        email: email,
-                        password: hashedPassword,
-                        cart: { items: [] }
-                    });
-                    return user.save();
-                })
-                .then(result => {
-                    res.redirect('/project/01/login');
-                    const url = req.hostname == 'localhost' ? `http://${req.hostname}:${process.env.PORT}/project/01/login` : `https://${req.hostname}/project/01/login`;
-                    return transporter.sendMail({
-                        to: email,
-                        from: 'PrintShop^3 <ch@ndlerchilds.net>',
-                        subject: "Welcome to PrintShop^3",
-                        html: `<h1>You successfully signed up!</h1><p>Welcome ${email}!</p><p>Log In Here: <a href="${url}">${url}</a></p>`
-                    })
-                })
-                .catch((err) => next(new Error(err)));
+  User.findOne({ email: email })
+    .then((userDoc) => {
+      if (userDoc) {
+        req.flash("error", "E-Mail already in use");
+        return res.redirect("/project/01/signup");
+      }
+      if (password !== confirmPassword) {
+        return res.redirect("/project/01/signup");
+      }
+      return bcrypt
+        .hash(password, 12)
+        .then((hashedPassword) => {
+          const user = new User({
+            email: email,
+            password: hashedPassword,
+            cart: { items: [] },
+          });
+          return user.save();
+        })
+        .then((result) => {
+          res.redirect("/project/01/login");
+          const url =
+            req.hostname == "localhost"
+              ? `http://${req.hostname}:${process.env.PORT}/project/01/login`
+              : `https://${req.hostname}/project/01/login`;
+          return transporter.sendMail({
+            to: email,
+            from: "PrintShop^3 <ch@ndlerchilds.net>",
+            subject: "Welcome to PrintShop^3",
+            html: `<h1>You successfully signed up!</h1><p>Welcome ${email}!</p><p>Log In Here: <a href="${url}">${url}</a></p>`,
+          });
         })
         .catch((err) => next(new Error(err)));
+    })
+    .catch((err) => next(new Error(err)));
 };
 
 exports.postLogout = (req, res, next) => {
-    req.session.destroy(err => {
-        if (err) next(new Error(err));
-        res.redirect('/project/01/');
-    });
+  req.session.destroy((err) => {
+    if (err) next(new Error(err));
+    res.redirect("/project/01/");
+  });
+};
+
+exports.getReset = (req, res, next) => {
+  res.render("project-views/project-01/auth/reset", {
+    title: "Reset Password",
+    path: "/reset",
+    errorMessages: req.flash("error"),
+  });
 };
--- routes/project-routes/project-01/auth.js
+++ routes/project-routes/project-01/auth.js
@@ -3,9 +3,16 @@ const router = require('express').Router();
 const authController = require('../../../controllers/project-controllers/project-01/auth')
 
 router.get('/login', authController.getLogin);
-router.get('/signup', authController.getSignup);
 router.post('/login', authController.postLogin);
+
+router.get('/signup', authController.getSignup);
 router.post('/signup', authController.postSignup);
+
 router.post('/logout', authController.postLogout);
+
+router.get('/reset', authController.getReset);
+// router.post('/reset', authController.postReset);
+// router.get('/reset/:token', authController.getNewPassword);
+// router.post('/new-password', authController.postNewPassword);
 
 module.exports = router;
