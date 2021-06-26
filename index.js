6aadb27a9a705c2cc282c9571577ab5213e74158
Chandler Childs <chandler@advenna.com>
2021-06-26 15:03:53 -0600

    add prove 10

--- index.js
+++ index.js
@@ -56,6 +56,13 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 const options = {
   useUnifiedTopology: true,
   useNewUrlParser: true,
--- routes/prove-routes/index.js
+++ routes/prove-routes/index.js
@@ -4,6 +4,7 @@ router.use('/01', require('./prove-01-routes'));
 router.use('/02', require('./prove-02-routes'));
 router.use('/08', require('./prove-08-routes'));
 router.use('/09', require('./prove-09-routes'));
+router.use('/10', require('./prove-10-routes'));
 
 router.get('/', (req, res, next) => {
     res.render('index', { title: 'Prove Assignments', path: '/prove' });
